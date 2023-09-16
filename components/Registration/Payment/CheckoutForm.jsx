2de18deae541d4db361a8569614f727eab61e455
AHSAN RAZA <ahsan.raza@studentambassadors.com>
2023-09-17 00:11:08 +0500

    fix(app): redirct page after payment

--- components/Registration/Payment/CheckoutForm.jsx
+++ components/Registration/Payment/CheckoutForm.jsx
@@ -60,7 +60,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: 'http://localhost:3000/success',
+        return_url: 'https://ucr.vercel.app/success',
       },
     });
 
