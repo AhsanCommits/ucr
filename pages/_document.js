fc5b45e9367a88fd9939aabbcaf25eaed773689d
Hassan <ghassanpk@gmail.com>
2023-09-22 00:03:28 +0500

    updated title

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { Separator } from '@/components/ui/separator';
+import { Html, Head, Main, NextScript } from "next/document";
+import { Separator } from "@/components/ui/separator";
 
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>UCR.gg</title>
+      </Head>
       <body>
         <Navbar />
         <Separator />
