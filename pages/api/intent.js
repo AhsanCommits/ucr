de1b7e65912975ea915c7a01db59f7ddd86b4858
Hassan <ghassanpk@gmail.com>
2023-09-21 21:45:00 +0500

    save to airtable

--- pages/api/intent.js
+++ pages/api/intent.js
@@ -3,8 +3,79 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2020-08-27",
 });
 
+const getClassification = (formData) => {
+  let classification = [];
+
+  if (formData.broker) {
+    classification.push("Broker");
+  }
+
+  if (formData.freightForwarder) {
+    classification.push("Freight Forwarder");
+  }
+
+  if (formData.leasingCompany) {
+    classification.push("Leasing Company");
+  }
+
+  if (formData.motorCarrier) {
+    classification.push("Motor Carrier");
+  }
+
+  if (formData.motorPrivateCarrier) {
+    classification.push("Motor Private Carrier");
+  }
+
+  return classification.join(", ");
+};
+
 const saveToAirtable = async (formData, carrierData) => {
-  return "airtableId";
+  const obj = {
+    "Certificate?": formData.certificationNeeded,
+    Classification: getClassification(formData),
+    "Number of Units": `${carrierData.totalPowerUnits}`,
+    Street: carrierData.phyStreet,
+    "Processing Time": formData.processingTime,
+    "DOT Number": carrierData.dotNumber,
+    "Customer Certified": true,
+    "Who is creating the registration?": formData.fullName,
+    Email: formData.email,
+    "Digital Signature": formData.signature,
+    "Registration Year": formData.registrationYear,
+    State: carrierData.phyState,
+    "Zip Code": carrierData.phyZipcode,
+    "Carrier Name": carrierData.legalName,
+    City: carrierData.phyCity,
+    "Payment Status": "Pending",
+    Amount: makeAmount(carrierData) / 100,
+  };
+
+  // console.log("saving to airtable", obj);
+
+  try {
+    const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_NAME}`;
+    // console.log("url", url, process.env.AIRTABLE_API_KEY);
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        records: [
+          {
+            fields: obj,
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    // console.log("airtable response", data);
+    return data.records[0].id;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
 const makeAmount = (carrierData) => {
